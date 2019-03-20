1ab9e7e5ed37e1b37dd95ea97ac31d08721e3297
Krane <kranemora@gmail.com>
2019-03-20 16:20:42 -0500

    Operative

--- src/Installer/PluginAppInstaller.php
+++ src/Installer/PluginAppInstaller.php
@@ -3,7 +3,28 @@
 namespace Custom\Composer\Installer;
 
 use Composer\Installer\LibraryInstaller;
+use Composer\Package\PackageInterface;
 
 class PluginAppInstaller extends LibraryInstaller
 {
+    public function getInstallPath(PackageInterface $package)
+    {
+        $extra = $package->getExtra();
+        list(, $name) = explode("/", $package->getPrettyName());
+        $name = str_replace(
+            ' ',
+            '',
+            ucwords(str_replace(['-', '_'], [' ', ' '], $name))
+        );
+        if (empty($extra['vendor'])) {
+            return 'plugins' . DIRECTORY_SEPARATOR . $name;
+        } else {
+            return $extra['vendor'] . DIRECTORY_SEPARATOR . $name;
+        }
+    }
+
+    public function supports($packageType)
+    {
+        return 'cakephp-app-plugin' === $packageType;
+    }
 }
